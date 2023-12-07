691b0bce539a6332b053ad13ac1ec25dccd5e335
VdVSander <sander.vandeven86@gmail.com>
2023-12-07 20:24:59 +0100

    Devided the large program into subroutines.

--- rotate_pdf.py
+++ rotate_pdf.py
@@ -5,56 +5,100 @@ import pdf2image
 from PIL import Image
 import PIL
 
-#Definition static variables
-pdfFiles = []
-
-#Check the entered arguments
-try:
-    #Show the manual
-    if sys.argv[1] == "--man" or sys.argv[1] == "-m" or sys.argv[1] == "m" or sys.argv[1] == "manual":
-        print("Usage: python3 crop.py <path> or [--all/-a] for all files in the current directory")
-        sys.exit()
-    #Get all PDF files from the current working directory
-    elif sys.argv[1] == "--all" or sys.argv[1] == "-a":
-        dir_content = os.listdir()
-        for file in dir_content:
-            if file[-4:] == ".pdf" and os.path.isfile(file):
-                pdfFiles.append(file)
-        if not pdfFiles:
-            print("No PDF files were found in this directory")
+def check_input(arg):
+    pdfFiles = []
+    #Check the entered arguments
+    try:
+        #Show the manual
+        if arg == "--man" or arg == "-m" or arg == "m" or arg == "manual":
+            print("Usage: python3 crop.py <path> or [--all/-a] for all files in the current directory")
+            sys.exit()
+        #Get all PDF files from the current working directory
+        elif arg == "--all" or arg == "-a":
+            dir_content = os.listdir()
+            for file in dir_content:
+                if file[-4:] == ".pdf" and os.path.isfile(file):
+                    pdfFiles.append(file)
+            if not pdfFiles:
+                print("No PDF files were found in this directory")
+            else:
+                print("We found " + str(len(pdfFiles)) + " PDF file(s).")
+        #Get the PDF file from the path
+        elif os.path.isfile(arg) and arg[-4:] == ".pdf":
+            pdfFiles.append(arg)
         else:
-            print("We found " + str(len(pdfFiles)) + " PDF file(s).")
-    #Get the PDF file from the path
-    elif os.path.isfile(sys.argv[1]) and sys.argv[1][-4:] == ".pdf":
-        pdfFiles.append(sys.argv[1])
-    else:
+            print("Please enter a valid path to a PDF file or use the --man argument for more information.")
+            sys.exit()
+    except:
         print("Please enter a valid path to a PDF file or use the --man argument for more information.")
         sys.exit()
-except:
-    print("Please enter a valid path to a PDF file or use the --man argument for more information.")
-    sys.exit()
+    return pdfFiles
 
-dir_name = "temp"
-# Check if the directory already exists
-if not os.path.exists(dir_name):
-    # If it doesn't exist, create it
-    os.makedirs(dir_name)
+def check_temp_dir():
+    dir_name = "temp"
+    # Check if the directory already exists
+    if not os.path.exists(dir_name):
+        # If it doesn't exist, create it
+        os.makedirs(dir_name)
 
-for x, filename in enumerate(pdfFiles):
-    #Open the file to read
-    images = pdf2image.convert_from_path(filename)
-    for i in range(len(images)):
-        print("Page " + str(i+1) + " / " + str(len(images)))
-        images[i].save('temp/page'+str(i)+'.png', 'PNG')
-    images = []
-    for path in os.listdir("temp/"):
-        if os.path.isfile(os.path.join("temp/", path)):
-            images.append(path)
-image_paths = []
+def pdf_to_png(pdfFiles):
+    #Repeat for every PDF file
+    for x, filename in enumerate(pdfFiles):
+        print(filename)
+        #Convert the PDF file to images of every page
+        images = pdf2image.convert_from_path(filename)
+        #Save every page in the temporary directory
+        for i in range(len(images)):
+            print("Page " + str(i+1) + " / " + str(len(images)))
+            images[i].save('temp/page'+str(i)+'.png', 'PNG')
+        #Clear the list again for the next PDF file
+        images = []
+        #
+        for path in os.listdir("temp/"):
+            if os.path.isfile(os.path.join("temp/", path)):
+                images.append(os.path.join("temp/", path))
+    return(images)
 
+def rotate_image(im, degrees):
+    im = im.rotate(degrees, PIL.Image.NEAREST, expand = 1)
+    im.show()
+    answer = input("OK? (y/n): ")
+    im.save(image)
+    return(im, answer)
+
+def png_to_pdf():
+    #Open the images and then convert them back to PDF
+    open_images = [Image.open(image_path) for image_path in images]
+    converted_images = [image.convert('RGB') for image in open_images]
+    return(converted_images)
+
+def save_pdf_files(converted_images):
+    #Save the PDF files
+    title = input("Please enter the title of the piece: ")
+    converted_images[0].save("./" + title + ".pdf", save_all=True, append_images=converted_images)
+
+def remove_temp_files():
+    # Get a list of all files in the temporary directory
+    file_list = os.listdir("./temp/")
+
+    # Iterate through the files and remove them
+    for file_name in file_list:
+        file_path = os.path.join("./temp/", file_name)
+        
+        # Check if the path is a file (not a directory)
+        if os.path.isfile(file_path):
+            os.remove(file_path)
+
+# -- MAIN PROGRAM -- #
+
+pdfFiles = check_input(sys.argv[1])
+check_temp_dir()
+images = pdf_to_png(pdfFiles)
+
+#Iterate over every page and rotate it until it's as desired
 for image in images:
-    im = Image.open("./temp/" + image)
-    image_paths.append("./temp/" + image)
+    #Show the image of the page
+    im = Image.open(image)
     im.show()
     answer = 'n'
     while answer != 'y':
@@ -62,48 +106,16 @@ for image in images:
         print("Please close the previous window.")
         if(degrees == "l"):
             degrees = 90
-            im = im.rotate(degrees, PIL.Image.NEAREST, expand = 1)
-            im.show()
-            answer = input("OK? (y/n): ")
-            im.save("./temp/" + image)
+            im, answer = rotate_image(im, degrees)
         elif(degrees == "r"):
             degrees = -90
-            im = im.rotate(degrees, PIL.Image.NEAREST, expand = 1)
-            im.show()
-            answer = input("OK? (y/n): ")
-            im.save("./temp/" + image)
+            im, answer = rotate_image(im, degrees)
         elif(degrees.isnumeric()):
             degrees = int(degrees)
-            im = im.rotate(degrees, PIL.Image.NEAREST, expand = 1)
-            im.show()
-            answer = input("OK? (y/n): ")
-            im.save("./temp/" + image)
+            im, answer = rotate_image(im, degrees)
         else:
             print("Please enter a valid response (l,r or °)")
             answer = "n"
-            
 
-open_images = []
-for image_path in image_paths:
-    opened_image = Image.open(image_path)
-    open_images.append(opened_image)
-
-converted_images = []
-for image in open_images:
-    converted_image = image.convert('RGB')
-    converted_images.append(converted_image)
-
-title = input("Please enter the title of the piece: ")
-converted_images[0].save("./" + title + ".pdf", save_all=True, append_images=converted_images)
-
-# Get a list of all files in the temporary directory
-file_list = os.listdir("./temp/")
-
-# Iterate through the files and remove them
-for file_name in file_list:
-    file_path = os.path.join("./temp/", file_name)
-    
-    # Check if the path is a file (not a directory)
-    if os.path.isfile(file_path):
-        os.remove(file_path)
-        print(f"Removed file: {file_name}")
+save_pdf_files(png_to_pdf())
+remove_temp_files()
